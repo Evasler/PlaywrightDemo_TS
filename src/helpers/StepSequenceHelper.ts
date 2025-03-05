4ea0920075b810ad28fe7d1c9a27af8c87c8c019
Evasler <chr.krokos@gmail.com>
2025-03-05 02:39:44 +0200

    Improved StepSequenceHelper to produce error.stack, that points to the proper method call in .spec file, upon failure

--- src/helpers/StepSequenceHelper.ts
+++ src/helpers/StepSequenceHelper.ts
@@ -1,10 +1,44 @@
+import config from "../../playwright.config";
+
 export class StepSequenceHelper {
     
+    private _testFilePath: RegExp;
+    private _testFileStepCall: string | undefined;
     private _stepSequence: Promise<void> = Promise.resolve();
     
+    constructor() {
+        this._testFilePath = new RegExp(`${config.testDir!.replaceAll(/\./g, "").replaceAll(/\\|\//g, "(\\\\|/)")}(\\\\|/).+\.spec\.ts:[0-9]+:[0-9]+$`);
+    }
+
     get stepSequence() { return this._stepSequence; }
 
+    private testFileStepCall(callstack: string) {
+        const testErrorRows = callstack!.split('\n').filter(row => this._testFilePath.test(row));
+        if (testErrorRows.length === 0)
+            throw new Error(`Found 0 rows matching ${this._testFilePath}`);
+        else if (testErrorRows.length > 1)
+            throw new Error(`Found multiple rows matching ${this._testFilePath}`);
+        return testErrorRows[0];
+    }
+
+    private populateTestFileStepCall(callstack: string) {
+        if (!this._testFileStepCall)
+            this._testFileStepCall = this.testFileStepCall(callstack);
+    }
+
+    private overwriteTestFileFunctionCall(callstack: string) {
+        if (!this._testFileStepCall)
+            throw new Error("Test row that caused the Test failure is unknown");
+        callstack = callstack.replace(this.testFileStepCall(callstack), this._testFileStepCall);
+        return callstack;
+    }
+
     addStep(step: () => Promise<void>) {
-        this._stepSequence = this._stepSequence.then(step);
+        const myError = new Error();
+        this._stepSequence = this._stepSequence.then(step).catch(error => {
+            this.populateTestFileStepCall(myError.stack!);
+            error.stack = this.overwriteTestFileFunctionCall(error.stack!);
+            throw error;
+        });
     }
 }

5ae3e12eabfa5703447543ff23b06ca9fd6040cb
Evasler <chr.krokos@gmail.com>
2025-08-06 02:38:29 +0300

    Added enforce-unique. Applied enforce-unique to createRoomPayload

--- resources/payloads/createRoomPayload.ts
+++ resources/payloads/createRoomPayload.ts
@@ -5,11 +5,15 @@ import {
 } from "../../src/types/index.js";
 import { faker } from "@faker-js/faker";
 import { generalUtils } from "../../src/utils/index.js";
+import { UniqueEnforcer } from "enforce-unique";
+
+const uniqueEnforcer = new UniqueEnforcer();
+
 export function createRoomPayload(
   hardData: Partial<CreateRoomPayload> = {},
 ): CreateRoomPayload {
   const {
-    roomName = `RM. ${generalUtils.padNumber(faker.number.int({ min: 1, max: 999 }), 3)}`,
+    roomName = `RM. ${generalUtils.padNumber(uniqueEnforcer.enforce(() => faker.number.int({ min: 1, max: 999 })), 3)}`,
     type = faker.helpers.arrayElement(roomType),
     accessible = faker.datatype.boolean(),
     description = faker.lorem.paragraph(),

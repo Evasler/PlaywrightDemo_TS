09993de61159765ef2dc738bb39e3616a1d2ae57
Evasler <chr.krokos@gmail.com>
2025-09-04 09:56:43 +0300

    Removed comments from playwright.config.ts. Added viewport, reducedMotion and serviceWorkers options

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,14 +3,6 @@ import type { ErrorListenerOptions } from "./src/types/index.js";
 import dotenv from "dotenv";
 import { interProcessCommunicationHelper } from "./src/helpers/data/interProcessCommunicationHelper.js";
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
 dotenv.config({ path: ".env", quiet: true });
 if (!process.env.GREP)
   throw new Error(
@@ -19,21 +11,13 @@ if (!process.env.GREP)
 
 await interProcessCommunicationHelper.setup();
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig<{ errorListenerOptions: ErrorListenerOptions }>({
   testDir: "./tests",
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.RETRIES ? +process.env.RETRIES : 0,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.WORKERS ? +process.env.WORKERS : 1,
   reportSlowTests: null,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ["list"],
     ["./src/reporters/GlobalReporter.ts"],
@@ -64,12 +48,7 @@ export default defineConfig<{ errorListenerOptions: ErrorListenerOptions }>({
     //'./src/reporters/ExecOrderReporter.ts'
   ],
   expect: { timeout: 10000 },
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "retain-on-first-failure",
     baseURL: "https://automationintesting.online/",
     errorListenerOptions: {
@@ -78,9 +57,13 @@ export default defineConfig<{ errorListenerOptions: ErrorListenerOptions }>({
       failOnRequestError: true,
     },
     headless: process.env.HEADLESS === "true",
+    viewport: { width: 1920, height: 1080 },
+    contextOptions: {
+      reducedMotion: "reduce",
+      serviceWorkers: "block",
+    },
   },
 
-  /* Configure projects for major browsers */
   projects: [
     {
       name: "chromium-prod",
@@ -106,46 +89,5 @@ export default defineConfig<{ errorListenerOptions: ErrorListenerOptions }>({
       name: "executionOrderTeardown",
       testMatch: /tests\/executionOrder\/dependencies\/global\.teardown\.ts/,
     },
-    // {
-    //   name: 'chromium',
-    //   use: { ...devices['Desktop Chrome'] },
-    // },
-
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
